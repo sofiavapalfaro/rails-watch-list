8c983b370a9877f51c8fd9aa21d9ff0c819a758d
(Sofia Alfaro) sofiavapalfaro98@gmail.com <sofiavapalfaro98@gmail.com>
2023-12-14 15:27:01 -0500

    Watch List

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,39 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to list_path(@list)
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+    @list.update(list_params)
+    redirect_to list_path(@list)
+  end
+
+  def destroy
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+end
